82ba73b6d62f12c75bc30e83ce273deb9a966ddc
hkastler <hkastler+github@gmail.com>
2017-04-10 20:10:02 -0500

    change to dateformatter

--- reeler/src/main/java/com/hkstlr/reeler/weblogger/weblogs/control/DateFormatter.java
+++ reeler/src/main/java/com/hkstlr/reeler/weblogger/weblogs/control/DateFormatter.java
@@ -19,13 +19,30 @@ public class DateFormatter {
     public DateFormatter() {
     }
     
+    /**
+     * yyyy MMM dd HH:mm
+     */
     public static DateFormat sdf = new SimpleDateFormat("yyyy MMM dd HH:mm");
+    
+    /**
+     * yyyy
+     */
     public static DateFormat yearFormat = new SimpleDateFormat("yyyy");
+    
+    /**
+     * MM
+     */
     public static DateFormat monthFormat = new SimpleDateFormat("MM");
     public static DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
     public static DateFormat dayFormat = new SimpleDateFormat("d");
     public static DateFormat format8chars = new SimpleDateFormat("YYYYMMDD");
     public static DateFormat yearMonthFormat = new SimpleDateFormat("yyyyMMM");
+    
+    /**
+     * yyyy-MM-dd'T'HH:mm:ss.SSSXXX
+     * thanks to https://github.com/jarrodhroberson/Stack-Overflow/blob/master/src/main/java/com/stackoverflow/Q2597083.java
+     */
+    public static DateFormat jsFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
     
     
     static public DateFormat localeDefaultDateFormat(Locale currentLocale) {
