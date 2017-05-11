9e1acba6051d58aa5b553238771e8d80fed99074
hkastler <hkastler+github@gmail.com>
2017-05-11 10:41:07 -0500

    del template code to improve sonarqube numbers;may reintroduce later

--- reeler/src/main/java/com/hkstlr/reeler/weblogger/themes/control/RenditionType.java
+++
@@ -1,5 +0,0 @@
-package com.hkstlr.reeler.weblogger.themes.control;
-
-public enum RenditionType {
-	STANDARD, MOBILE
-}
--- reeler/src/main/java/com/hkstlr/reeler/weblogger/themes/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author henry.kastler
- *
- */
-package com.hkstlr.reeler.weblogger.themes;
