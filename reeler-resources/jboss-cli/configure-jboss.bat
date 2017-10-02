8d54c65d2f6334eb47cebbaca20ede44509f9692
Henry <hkastler+github@gmail.com>
2017-10-02 08:37:07 -0500

    reeler dockerization

--- reeler-resources/jboss-cli/configure-jboss.bat
+++ reeler-resources/jboss-cli/configure-jboss.bat
@@ -1,8 +1,8 @@
 @echo off
 cls
-call \apps\wildfly\wildfly-10.1.0.Final\bin\jboss-cli --connect --file=configure-logging.cli
+call \apps\wildfly\wildfly-10.1.0.Final\bin\jboss-cli --user=admin --password=adminadmin1 --connect --file=configure-logging.cli
 pause
-call \apps\wildfly\wildfly-10.1.0.Final\bin\jboss-cli --connect --file=configure-postgresql.cli
+call \apps\wildfly\wildfly-10.1.0.Final\bin\jboss-cli --user=admin --password=adminadmin1  --connect --file=configure-postgresql.cli
 pause
-call \apps\wildfly\wildfly-10.1.0.Final\bin\jboss-cli --connect --file=configure-mail.cli
+call \apps\wildfly\wildfly-10.1.0.Final\bin\jboss-cli --user=admin --password=adminadmin1  --connect --file=configure-mail.cli
 
