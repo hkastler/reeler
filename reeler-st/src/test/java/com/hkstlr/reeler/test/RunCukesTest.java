6af1d6d4babc42323ff6f26e79e969f19e83fc31
Henry <hkastler+github@gmail.com>
2017-10-10 12:47:40 -0500

    cucumber test changes

--- reeler-st/src/test/java/com/hkstlr/reeler/test/RunCukesTest.java
+++ reeler-st/src/test/java/com/hkstlr/reeler/test/RunCukesTest.java
@@ -5,7 +5,10 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
+@CucumberOptions(plugin = {"pretty","html:target/cucumber" ,
+                           "json:target/cucumber.json" ,
+                           "junit:target/cucumber.xml"}
+                            , features = { "src/test/resources/com/hkstlr/reeler/test/login.feature" })
 public class RunCukesTest {
 }
 
--- reeler-st/src/test/java/com/hkstlr/reeler/test/pageobject/LoginPage.java
+++ reeler-st/src/test/java/com/hkstlr/reeler/test/pageobject/LoginPage.java
@@ -1,5 +1,8 @@
 package com.hkstlr.reeler.test.pageobject;
 
+import com.hkstlr.reeler.test.util.TestUtils;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,8 +11,12 @@ import org.openqa.selenium.support.ui.LoadableComponent;
 
 public class LoginPage extends LoadableComponent<LoginPage> {
 
+    private static final Logger LOG = Logger.getLogger(LoginPage.class.getName());
+
+    
+    
     private final WebDriver driver;
-    private final String pageURL = "http://localhost:8080/reeler/weblogger/login";
+    private final String pageURL = TestUtils.getTestURL() + "/reeler/weblogger/login";
 
     @FindBy(id = "login")
     public WebElement loginForm;
@@ -21,6 +28,8 @@ public class LoginPage extends LoadableComponent<LoginPage> {
     public WebElement passwordField;
 
     public LoginPage(WebDriver aDriver) {
+        LOG.info(pageURL);
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF); 
         this.driver = aDriver;
         PageFactory.initElements(driver, this);
         this.driver.get(pageURL);
