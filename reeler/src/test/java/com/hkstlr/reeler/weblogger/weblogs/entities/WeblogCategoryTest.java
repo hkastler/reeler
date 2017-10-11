b00b1852dc1a3d41ec0734eb57b6b642d0de8cbd
Henry <hkastler+github@gmail.com>
2017-10-11 10:01:45 -0500

    fix test for new impl

--- reeler/src/test/java/com/hkstlr/reeler/weblogger/weblogs/entities/WeblogCategoryTest.java
+++ reeler/src/test/java/com/hkstlr/reeler/weblogger/weblogs/entities/WeblogCategoryTest.java
@@ -17,73 +17,69 @@ import org.junit.Before;
  * @author henry.kastler
  */
 public class WeblogCategoryTest {
-    
+
     WeblogCategory cut;
-    
+
     public WeblogCategoryTest() {
         //constructor
     }
-    
+
     @Before
-    public void setUp() throws Exception {       
+    public void setUp() throws Exception {
         cut = new WeblogCategory();
     }
-    
+
     @Test
-    public void testWeblogCategoryEntity(){
+    public void testWeblogCategoryEntity() {
         TestEntityReflector ter = new TestEntityReflector();
         ter.testEntityClass(cut);
     }
-    
+
     @Test
-    public void testCalculatePosition() throws Exception{
-        
+    public void testCalculatePosition() throws Exception {
+
         Weblog weblog = TestSetup.getWeblog();
-        
-        WeblogCategory newCat = new WeblogCategory(weblog, "Name",
-                weblog.getWeblogCategories().size());
-        
+
+        WeblogCategory newCat = new WeblogCategory(weblog, "Name");
+
         List<WeblogCategory> cats = new ArrayList<>();
         cats.add(newCat);
-        weblog.setWeblogCategories(cats);        
+        weblog.setWeblogCategories(cats);
         assertEquals(1, newCat.getPosition());
-        
-        WeblogCategory newCat2 = new WeblogCategory(weblog, "Name", weblog.getWeblogCategories().size());
-        
+
+        WeblogCategory newCat2 = new WeblogCategory(weblog, "Name");
+
         cats.add(newCat);
         cats.add(newCat2);
         weblog.setWeblogCategories(cats);
-        
+
         assertEquals(2, newCat2.getPosition());
-        
-        WeblogCategory newCat3 = new WeblogCategory(weblog, "position test",
-                weblog.getWeblogCategories().size());
-        
-        assertEquals(3, newCat3.getPosition());
-        
+
+        WeblogCategory newCat3 = new WeblogCategory(weblog, "position test", 10);
+
+        assertEquals(10, newCat3.getPosition());
+
         weblog = TestSetup.getWeblog();
-        
+
         String catstr = "General,Technology,Finance";
         int position = 1;
-        if (catstr != null) {
-            String[] splitcats = catstr.split(",");
-            
-            for (String catName : splitcats) {
-                if (catName.trim().length() == 0) {
-                    continue;
-                }
-                //System.out.println( blogCats.size());
-                WeblogCategory cat = new WeblogCategory(weblog, catName);
-                //System.out.println("position: " + position);
-                //System.out.println("categoryPosition: " + cat.getPosition());
-                
-                weblog.getWeblogCategories().add(cat);
-                assertEquals(cat.getPosition(), position);
-                position++;
+
+        String[] splitcats = catstr.split(",");
+
+        for (String catName : splitcats) {
+            if (catName.trim().length() == 0) {
+                continue;
             }
+            //System.out.println( blogCats.size());
+            WeblogCategory cat = new WeblogCategory(weblog, catName);
+            //System.out.println("position: " + position);
+            //System.out.println("categoryPosition: " + cat.getPosition());
+
+            weblog.getWeblogCategories().add(cat);
+            assertEquals(cat.getPosition(), position);
+            position++;
         }
+
     }
 
-    
-    
 }
