65eed9f2df2d9177b9004c385d0dd013d76652f1
Henry <hkastler+github@gmail.com>
2017-10-31 16:47:29 -0500

    click thru tests and ui changes

---
+++ reeler-st/src/test/java/com/hkstlr/reeler/test/selenium/BlogAdminClickThruIT.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hkstlr.reeler.test.selenium;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+/**
+ *
+ * @author Henry
+ */
+public class BlogAdminClickThruIT {
+    
+    HtmlUnitDriver wd;
+    
+    @Before
+    public void setUp() throws Exception {
+        wd = new HtmlUnitDriver();//new FirefoxDriver();
+        //wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void clickthru() {
+        wd.get("http://localhost:8080/reeler/weblogger/login/");
+        wd.findElement(By.id("username")).click();
+        wd.findElement(By.id("username")).clear();
+        wd.findElement(By.id("username")).sendKeys("testerson");
+        wd.findElement(By.id("password")).click();
+        wd.findElement(By.id("password")).clear();
+        wd.findElement(By.id("password")).sendKeys("password");
+        wd.findElement(By.id("submit")).click();
+        wd.findElement(By.id("nav-entry")).click();
+        
+        
+        wd.findElement(By.id("nav-entries")).click();
+        Assert.assertTrue(getHeadingText().contains("Entries"));
+        
+        wd.findElement(By.id("nav-comments")).click();
+        Assert.assertEquals("Comments", getHeadingText());
+        
+        wd.findElement(By.id("nav-categories")).click();
+        Assert.assertEquals("Categories", getHeadingText());
+        
+        wd.findElement(By.id("nav-blogrolls")).click();
+        Assert.assertEquals("Bookmarks", getHeadingText());
+        
+        wd.findElement(By.id("nav-mediafiles")).click();
+        
+        wd.findElement(By.linkText("Preferences")).click();
+        
+        wd.findElement(By.id("nav-config")).click();
+        Assert.assertEquals("Blog Settings", getHeadingText());
+        
+        wd.findElement(By.id("nav-members")).click();
+        
+        wd.findElement(By.id("nav-pings")).click();
+        
+        wd.findElement(By.id("nav-maintenance")).click();
+        
+    }
+    
+    private String getHeadingText(){
+        String heading = wd.findElement(By.cssSelector(
+                ".panel-primary > div:nth-child(2) > h2:nth-child(1)")).getText();
+        return heading;
+    }
+    
+    @After
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
---
+++ reeler-st/src/test/java/com/hkstlr/reeler/test/selenium/ServerAdminClickThruIT.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hkstlr.reeler.test.selenium;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+/**
+ *
+ * @author Henry
+ */
+public class ServerAdminClickThruIT {
+    
+    HtmlUnitDriver wd;
+    
+    @Before
+    public void setUp() throws Exception {
+        wd = new HtmlUnitDriver();//new FirefoxDriver();
+        //wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void clickthru() {
+        wd.get("http://localhost:8080/reeler/weblogger/login/");
+        wd.findElement(By.id("username")).click();
+        wd.findElement(By.id("username")).clear();
+        wd.findElement(By.id("username")).sendKeys("testerson");
+        wd.findElement(By.id("password")).click();
+        wd.findElement(By.id("password")).clear();
+        wd.findElement(By.id("password")).sendKeys("password");
+        wd.findElement(By.id("submit")).click();
+        
+        wd.get("http://localhost:8080/reeler/weblogger/reeler-ui/");
+        wd.findElement(By.id("navbarReeler")).click();
+        wd.findElement(By.linkText("Reeler UI")).click();
+        wd.findElement(By.linkText("Server Administration")).click();
+        wd.findElement(By.linkText("Configuration")).click();
+        Assert.assertEquals("Server Configuration", getHeadingText());
+        
+        wd.findElement(By.linkText("User Admin")).click();
+        wd.findElement(By.linkText("Global Comment Manager")).click();
+        wd.findElement(By.linkText("Ping Target")).click();
+        wd.findElement(By.linkText("Cache Info")).click();
+        
+    }
+    
+    private String getHeadingText(){
+        String heading = wd.findElement(By.cssSelector(
+                "#globalConfig > h2:nth-child(2)")).getText();
+        return heading;
+    }
+    
+    @After
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
